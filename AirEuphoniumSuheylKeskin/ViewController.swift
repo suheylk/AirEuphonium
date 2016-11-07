b6d7604828bd852dd856c89e5caa346c7548c2ff
suheyl keskin <skeskin@suheyls-macbook-pro.local>
2016-11-07 05:27:44 -0800

    Completed Exercise 3

--- AirEuphoniumSuheylKeskin/ViewController.swift
+++ AirEuphoniumSuheylKeskin/ViewController.swift
@@ -7,11 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var hornSound: AVAudioPlayer?
+    
+    func loadSound(name: String) -> AVAudioPlayer? {
+        
+        let bundle = Bundle.main
+        
+        let optionalPath = bundle.path(forResource: name, ofType: "m4a")
+        
+        if let path = optionalPath {
+           
+            let url = URL(fileURLWithPath: path)
+            
+            let optionalPlayer = try? AVAudioPlayer(contentsOf: url)
+           
+            if let player = optionalPlayer {
+               
+                player.prepareToPlay()
+                
+                print("\(name) loaded.")
+                
+                return player
+            }
+            else {
+                print("Could not load \(name). ")
+                
+                return nil
+            }
+        }
+        else {
+            print("Could not create path")
+            return nil
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.hornSound = loadSound(name: "hornSound")
+    }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
-        print("Hello, world!")
+        self.hornSound?.play()
     }
     
 }
